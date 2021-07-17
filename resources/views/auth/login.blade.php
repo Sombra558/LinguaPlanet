f7a5ae8295beb21990748ef5e5a0563ba8771896
Sistemas Cerízeon <sistemascerizeon@gmail.com>
2021-07-16 21:45:25 -0400

    eliminado welcome

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 <div class="container">
+<h1>{{ __('messages.title-login') }}</h1>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-            <h1>{{ __('messages.title-login') }}</h1>
+           
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
