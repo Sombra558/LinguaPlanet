c7669031051b2319084dfb5ad630c9747d4b6e13
German <hasker_75@hotmail.com>
2021-07-21 16:06:12 -0430

    no message

--- resources/views/Padres/Home/home.blade.php
+++ resources/views/Padres/Home/home.blade.php
@@ -1,22 +1,21 @@
-@extends('layouts.Padres.app')
+@extends('layouts.Padres.padre')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-12 text-center">
-        <h2 class="bold mt-4">Crea un perfil de estudiante</h2>
+        <h3 class="bold mt-4">Crea un perfil de estudiante</h3>
         </div>
     </div>
-    <div class="row d-flex align-items-center">
-            <div class="col-12 text-center">
+    <div class="row d-flex align-items-center pt-5">
+            <div class="col-12 text-center mt-5">
                 @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                 @endif
-                
                 <a href="/home/perfil-user/create">
-                    <img src="{{ asset('/storage/images/add.svg') }}">
+                    <img class="iz-10" src="{{ asset('/storage/images/add.svg') }}">
                 </a>
             </div>
     </div>    
