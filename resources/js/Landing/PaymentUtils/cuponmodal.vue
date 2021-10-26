52d7bc627d1b90562e2e38d03d958179628c5f24
German <hasker_75@hotmail.com>
2021-10-26 00:11:23 -0430

    Update: modal de ingreso de cupones

--- resources/js/Landing/PaymentUtils/cuponmodal.vue
+++ resources/js/Landing/PaymentUtils/cuponmodal.vue
@@ -1,22 +1,38 @@
 <template>
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content" style="height: 19rem;">
+                <div class="modal-header modal-header-admin pb-0">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22.5 7.5L7.5 22.5" stroke="#606060" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M7.5 7.5L22.5 22.5" stroke="#606060" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </div>
+                <div class="modal-body pt-0 px-5">
+                    <div class="row mt-4">
+                        <div class="col text-center">
+                            <h3 class="modal-title fw-600" id="exampleModalLabel">Cupón de descuento</h3>
+                        </div>                        
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col">
+                            <input type="text" class="form-input input-primary" v-model="cupon" maxlength="20">    
+                        </div>                        
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col">
+                            <button type="button" class="btn btn-block btn-success" @click.prevent="aplicar(cupon)">
+                                <span class="h5 fw-500">
+                                    Aplicar Cupon
+                                </span>
+                            </button>    
+                        </div>                        
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">
-            <input type="text" v-model="cupon" maxlength="20">
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary" @click.prevent="aplicar(cupon)">Aplicar Cupon</button>
-        </div>
-        </div>
-    </div>
     </div>
 </template>
 
