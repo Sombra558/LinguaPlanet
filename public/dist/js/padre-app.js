7ad206d84a00d89d0ff5780f712a4f80188fba57
Sistemas Cerízeon <sistemascerizeon@gmail.com>
2021-11-11 20:02:34 -0400

    calificacion

--- public/dist/js/padre-app.js
+++ public/dist/js/padre-app.js
@@ -1925,7 +1925,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
-  name: "progresoSemanal",
+  name: "Calificacion",
   props: ['clase', 'actividades'],
   computed: {
     calificacion: function calificacion() {
